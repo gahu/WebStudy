01bcd1c2ab7c41f6e169562e811e30d6a06b962a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-01 13:25:25 +0000

    Deploy gahu/WebStudy to github.com/gahu/WebStudy.git:gh-pages

---
+++ main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var _hello = require('./hello');
+
+var _hello2 = _interopRequireDefault(_hello);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+(0, _hello2.default)();
+// const hello = require('./hello');
+
+// const {
+//   hi
+// } = hello;
+
+// hello();
+// hi();
+
+// commonjs동작 방식
+// var hello = require('./hello');
+// var hi = hello.hi;
